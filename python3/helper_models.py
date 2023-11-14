6650578fa0bc27feee93f0196edc96958a13b156
kumarama3 <129521998+kumarama3@users.noreply.github.com>
2023-11-14 12:43:39 -0500

    Update helper_models.py

--- python3/helper_models.py
+++ python3/helper_models.py
@@ -87,7 +87,7 @@ def process_audio(model, API_URL):
     else:
         out = transcribe_whisper(audio_clip_path,model)
     
-    prompt = ". Answer this in two or less sentences "
+    prompt = " ?"
     out += prompt
     if "dance" in out.lower():
         func = "Dance"
